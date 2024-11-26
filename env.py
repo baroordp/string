7e6f35d5f30cd90862d8e702d28bc69a14a4c520
baroordp <169070181+baroordp@users.noreply.github.com>
2024-11-26 19:18:28 +0530

    Update env.py

--- env.py
+++ env.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID", "20822386").strip()
 API_HASH = os.getenv("API_HASH", "4f0069e749e20d9c399447b4a34f5a45").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7627762738:AAH-FRVhGkP0R06AkRcKIuDK5OhbcItAc6k").strip()
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://sk3:sk3@cluster0.7jowr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").strip() # Not a necessary variable anymore but you can add to get stats
 MUST_JOIN = os.getenv("MUST_JOIN", "-1002450276019")
 

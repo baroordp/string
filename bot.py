6f1c1b101fc78d3a25365857b32cacb1f87854e2
baroordp <169070181+baroordp@users.noreply.github.com>
2024-11-26 19:29:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import logging
 from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
-
+from aiohttp import web
+from StringSessionBot import web_server
 
 logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
@@ -15,6 +16,22 @@ app = Client(
     in_memory=True,
     plugins={'root':'StringSessionBot'},
 )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        logging.info(f"{me.first_name} with for pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, Config.PORT).start()
+        self.id = me.id
+        self.username = me.username
+        self.first_name = me.first_name
+        self.set_parse_mode(ParseMode.DEFAULT)
+        text = "<b>๏[-ิ_•ิ]๏ ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ !</b>"
+        logging.info(text)
+        success = failed = 0
 
 
 if __name__ == "__main__":
